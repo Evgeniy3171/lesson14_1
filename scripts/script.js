6ce7dd4ea054aa0e8179c9c8bf3630090cde28cd
Evgeniy3171 <evgen915628@list.ru>
2021-05-03 08:27:39 +0300

    Добавил комментарии и пояснения

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,7 @@
 4) Добавить папку с уроком на свой GitHub */
 
 
-function DomElement (selector, height, width, bg, fontSize){
+function DomElement (selector, height, width, bg, fontSize){  //создаем класс DomElement
   this.selector = selector; 
   this.height = height; 
   this.width = width; 
@@ -41,25 +41,28 @@ function DomElement (selector, height, width, bg, fontSize){
 }
 
 
-DomElement.prototype.newElem = function () {  //добавляем метод в класс
+DomElement.prototype.newElem = function () {  //добавляем метод в класс DomElement
   let elem;
-  if (this.selector[0] === '.') {
-      elem = document.createElement('div');
-      elem.className = this.selector.slice(1);
+  if (this.selector[0] === '.') {             //Проверяем первый символ
+      elem = document.createElement('div');   //создаем блок если первый символ .
+      elem.className = this.selector.slice(1);//slice делает копию массива. Берем второй элемент массива и его именем называем класс элемента
   }
-  if (this.selector[0] === '#') {
-      elem = document.createElement('p');
-      elem.id = this.selector.slice(1);
+  if (this.selector[0] === '#') {             //Проверяем первый символ
+      elem = document.createElement('p');     //создаем параграф если первый символ #
+      elem.id = this.selector.slice(1);       //Берем второй элемент массива и его именем называем идентификатор элемента
   }
-  elem.style.cssText = `height: ${this.height}px;
+  //настраиваем стили элемента:
+  elem.style.cssText = `height: ${this.height}px;   
       width: ${this.width}px;
       background: ${this.bg};
       font-size: ${this.fontSize}px;`;
   return elem;
 };
 
+//через конструктор создаем обьекты
 let elemBlock = new DomElement('.block', 100, 200, 'green', 12);
 let elemParagraph = new DomElement('#best', 150, 400, 'red', 12);
 
+//Тестируем код
 document.body.appendChild(elemBlock.newElem());
 document.body.appendChild(elemParagraph.newElem());
